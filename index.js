58f9cfa2bab97f0ed4b57f7e3a88001dc26aa2f2
Valeti Abhishek <valetiabhishek@Valetis-MacBook-Air.local>
2026-04-18 10:54:56 +0530

    name chnaged

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
 
     const projects = document.querySelectorAll(".project-card");
-    const buttons = document.querySelectorAll(".source-btn");
 
     const projectDetails = [
         "Airport & Runway Detection\n\nYOLO-based model with 91.3% accuracy.\nTechnologies: Python, YOLO, OpenCV",
@@ -13,26 +12,9 @@ document.addEventListener("DOMContentLoaded", function () {
         "Hybrid Dehazing\n\nImage dehazing + object detection.\nTechnologies: Python, YOLO"
     ];
 
-    const links = [
-        "",
-        "https://github.com/Abhishek200418/Anti-Boredom-Hub-",
-        "https://github.com/Abhishek200418/BookMark",
-        "https://github.com/Abhishek200418/image-dehazing-object-detection"
-    ];
-
     projects.forEach((project, index) => {
         project.addEventListener("click", () => {
             alert(projectDetails[index]);
-        });
-    });
-
-    buttons.forEach((btn, index) => {
-        btn.addEventListener("click", (e) => {
-            e.stopPropagation();
-
-            if (links[index]) {
-                window.open(links[index]);
-            }
         });
     });
 
